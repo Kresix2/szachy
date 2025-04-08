212c041cc2cebb0fc691d77eeae8d3c0b548a52f
Kresix2 <Igngrabowski@gmail.com>
2025-04-08 19:32:08 +0200

    Newer update, logs work better

--- szachy_main.py
+++ szachy_main.py
@@ -3,20 +3,20 @@ class board:
         #//////////////////////
         #   board and details
         #//////////////////////
-        self.Bpawn =   0b00000000_11111111_00000000_01000010_00000000_00000000_00000000_00000000
-        self.Bknight = 0b01000010_00000000_00000000_00000000_00000000_00000000_00000000_00000000
-        self.Brook =   0b10000001_00000000_00000000_00010000_00000000_00000000_00000000_00000000
-        self.Bbishop = 0b00100100_00000000_00000000_00000000_00000000_00000000_00000000_00000000
-        self.Bqueen =  0b00010000_00000000_00000000_00000000_00000000_00000000_00000000_00000000
-        self.Bking =   0b00001000_00000000_00000000_00000000_00000000_00000000_00000000_00000000
-    
-        
-        self.Wpawn =   0b0000000000000000000000000000000000000000000000001111111100000000
-        self.Wknight = 0b0000000000000000000000000000000000000000000000000000000001000010
-        self.Wrook =   0b0000000000000000000000000000000000000000000000000000000010000001
-        self.Wbishop = 0b0000000000000000000000000000000000000000000000000000000000100100
-        self.Wqueen  = 0b0000000000000000000000000000000000000000000000000000000000010000
-        self.Wking  =  0b0000000000000000000000000000000000000000000000000000000000001000
+        self.bitboards = {
+            "Bpawn":   0b00000000_11111111_00000000_00000000_00000000_00000000_00000000_00000000,
+            "Bknight": 0b01000010_00000000_00000000_00000000_00000000_00000000_00000000_00000000,
+            "Brook":   0b10000001_00000000_00000000_00000000_00000000_00000000_00000000_00000000,
+            "Bbishop": 0b00100100_00000000_00000000_00000000_00000000_00000000_00000000_00000000,
+            "Bqueen":  0b00010000_00000000_00000000_00000000_00010000_00000000_00000000_00000000,
+            "Bking":   0b00001000_00000000_00000000_00000000_00000000_00000000_00000000_00000000,
+            "Wpawn":   0b0000000000000000000000000000000000000000000000001111111100000000,
+            "Wknight": 0b0000000000000000000000000000000000000000000000000000000001000010,
+            "Wrook":   0b0000000000000000000000000000000000000000000000000000000010000001,
+            "Wbishop": 0b0000000000000000000000000000000000000000000000000000000000100100,
+            "Wqueen":  0b0000000000000000000000000000000000000000000000000000000000010000,
+            "Wking":   0b0000000000000000000000000000000000000000000000000000000000001000,
+        }
         self.moves = {
              'Wpawn':  [512, 1280, 2560, 5120, 10240, 20480, 40960, 16384, 131072, 327680, 655360, 1310720, 2621440, 5242880, 10485760, 4194304, 33554432, 83886080, 167772160, 335544320, 671088640, 1342177280, 2684354560, 1073741824, 8589934592, 21474836480, 42949672960, 85899345920, 171798691840, 343597383680, 687194767360, 274877906944, 2199023255552, 5497558138880, 10995116277760, 21990232555520, 43980465111040, 87960930222080, 175921860444160, 70368744177664, 562949953421312, 1407374883553280, 2814749767106560, 5629499534213120, 11258999068426240, 22517998136852480, 45035996273704960, 18014398509481984, 144115188075855872, 360287970189639680, 720575940379279360, 1441151880758558720, 2882303761517117440, 5764607523034234880, 11529215046068469760, 4611686018427387904, 0, 0, 0, 0, 0, 0, 0, 0]
             ,'Bpawn':  [0, 0, 0, 0, 0, 0, 0, 0, 2, 5, 10, 20, 40, 80, 160, 64, 512, 1280, 2560, 5120, 10240, 20480, 40960, 16384, 131072, 327680, 655360, 1310720, 2621440, 5242880, 10485760, 4194304, 33554432, 83886080, 167772160, 335544320, 671088640, 1342177280, 2684354560, 1073741824, 8589934592, 21474836480, 42949672960, 85899345920, 171798691840, 343597383680, 687194767360, 274877906944, 2199023255552, 5497558138880, 10995116277760, 21990232555520, 43980465111040, 87960930222080, 175921860444160, 70368744177664, 562949953421312, 1407374883553280, 2814749767106560, 5629499534213120, 11258999068426240, 22517998136852480, 45035996273704960, 18014398509481984]
@@ -29,11 +29,13 @@ class board:
             ,'diagonal':     [72057594037927936, 144396663052566528, 288794425616760832, 577588855528488960, 1155177711073755136, 2310355422147575808, 4620710844295151872, 9241421688590303745, 36099303471055874, 141012904183812, 550831656968, 2151686160, 8405024, 32832, 128]
             ,'anti_diagonal':[1, 258, 66052, 16909320, 4328785936, 1108169199648, 283691315109952, 72624976668147840, 145249953336295424, 290499906672525312, 580999813328273408, 1161999622361579520, 2323998145211531264, 4647714815446351872, 9223372036854775808]
         }
-        #what white occupies
-        self.white_occ =  self.Wpawn|self.Wknight|self.Wrook|self.Wbishop|self.Wqueen|self.Wking
-        # what black occupies
-        self.black_occ = self.Bpawn|self.Bknight|self.Brook|self.Bbishop|self.Bqueen|self.Bking
-        self.occupied =  self.white_occ|self.black_occ
+        #what ocupies each of the sides, W => white, B => black
+        self.occupied_sides = {
+            'B':sum([bitboard  for name,bitboard in self.bitboards.items() if name.startswith('B')])
+            ,'W':sum([bitboard  for name,bitboard in self.bitboards.items() if name.startswith('W')])
+            }
+
+        self.occupied =  sum(self.occupied_sides.values())
         #/////////////////////////////
         #   move generation details
         #////////////////////////////
@@ -42,6 +44,7 @@ class board:
         self.H_file = 0b1111111011111110111111101111111011111110111111101111111011111110
         self.AB_file = 0b0011111100111111001111110011111100111111001111110011111100111111
         self.GH_file = 0b1111110011111100111111001111110011111100111111001111110011111100
+    
     @staticmethod
     def get_columns(start,end):
         lst = []
@@ -51,13 +54,8 @@ class board:
             else:
                 lst.append('1')
         return int(''.join(lst*8),2)
-    @staticmethod
-    def log_bitboard(board):
-        assert board& ~0xFFFFFFFFFFFFFFFF == 0
-        board = '{:064b}'.format(board).replace('0','.')
-        for i in range(0,8):
-            print(' '.join(board[i*8:i*8+8]))
-        print('')
+    
+    
     @staticmethod
     def format_bin(binaries):
         return_bin = []
@@ -66,13 +64,14 @@ class board:
         for binary in binaries:
             return_bin.append('{:064b}'.format(binary))
         return return_bin
+    
     def log_chessboard(self):
         printout = ['.']*64
-        white_names = ['♟','♞','♝','♜','♛','♚']
-        whites = [self.Wpawn,self.Wknight,self.Wrook,self.Wbishop,self.Wqueen,self.Wking]
+        white_names = ['♟','♞','♜','♝','♛','♚']
+        whites = [bitboard  for name,bitboard in self.bitboards.items() if name.startswith('W')]
         whites = self.format_bin(whites)
         black_names = ['♙','♘','♖','♗','♕','♔']
-        blacks = [self.Bpawn,self.Bknight,self.Brook,self.Bbishop,self.Bqueen,self.Bking]
+        blacks = [bitboard  for name,bitboard in self.bitboards.items() if name.startswith('B')]
         blacks = self.format_bin(blacks)
         for i,piece in enumerate(whites):
             for j in range(64):
@@ -86,19 +85,29 @@ class board:
         print('    A B C D E F G H')
         for i in range(8):
             print(8-i,'|',' '.join(printout[i*8:i*8+8]))
+    
     def possible_moves_cross(self,slider):
-        mask_hor = self.masks['cross_hor'][self.check_bits(slider)%8]
-        mask_ver = self.masks['cross_ver'][self.check_bits(slider)%8]
+        column = self.check_bits(slider)%8
+        row = self.check_bits(slider)//8
+        mask_hor = self.masks['cross_hor'][row]
+        mask_ver = self.masks['cross_ver'][column]
         return self.possible_moves_slider(mask_hor,slider)|self.possible_moves_slider(mask_ver,slider)&self.inside
     
-
     def possible_moves_diagonals(self,slider):
-
-        mask_diag = self.masks['diagonal'][self.check_bits(slider)%8+7-self.check_bits(slider)//8]
-        mask_antidiag = self.masks['anti_diagonal'][self.check_bits(slider)%8+self.check_bits(slider)//8]
-        return self.possible_moves_slider(mask_diag,slider)|self.possible_moves_slider(mask_antidiag,slider)
-        
+        slider_index = self.check_bits(slider)
+        mask_diag = self.masks['diagonal'][slider_index%8+7-slider_index//8]
+        mask_antidiag = self.masks['anti_diagonal'][slider_index%8+slider_index//8]
+        return self.possible_moves_slider(mask_diag,slider)|self.possible_moves_slider(mask_antidiag,slider)       
+    
     def possible_moves_slider(self,mask,slider):
+        def reverse_bitboard(b):
+            #from chess code exchanhge https://chess.stackexchange.com/questions/37309/move-generation-for-sliding-pieces-and-hyperbola-quintessence
+            b = (b & 0x5555555555555555) << 1 | ((b >> 1) & 0x5555555555555555)
+            b = (b & 0x3333333333333333) << 2 | ((b >> 2) & 0x3333333333333333)
+            b = (b & 0x0f0f0f0f0f0f0f0f) << 4 | ((b >> 4) & 0x0f0f0f0f0f0f0f0f)
+            b = (b & 0x00ff00ff00ff00ff) << 8 | ((b >> 8) & 0x00ff00ff00ff00ff)
+
+            return (b << 48) | ((b & 0xffff0000) << 16) | ((b >> 16) & 0xffff0000) | (b >> 48)
         assert slider&(slider-1) == 0
         #mask = self.masks['diagonal'][self.check_bits(slider)%8]
         #mask = self.masks['diagonal'][self.check_bits(slider)%8+8-self.check_bits(slider)//8]
@@ -108,34 +117,20 @@ class board:
         #masked occupied
         m_o = self.occupied&mask
         #reverse_bitboard mask occupied
-        m_oR = self.reverse_bitboard(self.occupied&mask)
+        m_oR = reverse_bitboard(self.occupied&mask)
 
         #slider pos
         s = slider
         #reverse_bitboardd slider pos
-        sR = self.reverse_bitboard(slider)
+        sR = reverse_bitboard(slider)
 
-        cross_attack = (m_o-2*s)^self.reverse_bitboard(m_oR-2*sR)
+        cross_attack = (m_o-2*s)^reverse_bitboard(m_oR-2*sR)
         cross_attack &= mask
          
         
         return cross_attack&~slider
 
-
-    def reverse_bitboard(self,b):
-        #from chess code exchanhge https://chess.stackexchange.com/questions/37309/move-generation-for-sliding-pieces-and-hyperbola-quintessence
-        b = (b & 0x5555555555555555) << 1 | ((b >> 1) & 0x5555555555555555)
-        b = (b & 0x3333333333333333) << 2 | ((b >> 2) & 0x3333333333333333)
-        b = (b & 0x0f0f0f0f0f0f0f0f) << 4 | ((b >> 4) & 0x0f0f0f0f0f0f0f0f)
-        b = (b & 0x00ff00ff00ff00ff) << 8 | ((b >> 8) & 0x00ff00ff00ff00ff)
-
-        return (b << 48) | ((b & 0xffff0000) << 16) | ((b >> 16) & 0xffff0000) | (b >> 48)
-
     
-
-
-        
-
     def generate_mask_cross(self):
         #generates masks needed for rook and queen sliding movement generation
 
@@ -164,6 +159,7 @@ class board:
             self.log_bitboard(horizontal)
         print(cross_mask_horizontal)
         print(cross_mask_vertical)
+    
     def generate_mask_diagonal(self):
         #generates masks needed for bishop and queen sliding movement generation
         #\
@@ -227,6 +223,7 @@ class board:
             self.log_bitboard(anti_diagonal)
         print(diagonal_mask)
         print(anti_diagonal_mask)
+    
     def generate_moves_pawn(self):
         A_file = 0b0111111101111111011111110111111101111111011111110111111101111111
         H_file = 0b1111111011111110111111101111111011111110111111101111111011111110
@@ -249,9 +246,11 @@ class board:
             b_pawn_lookup.append(temp)
             curr = curr<<1
         print(b_pawn_lookup)
+    
     def log_moves(self,name):
         for board in self.moves[name]:
             self.log_bitboard(board)
+    
     def generate_moves_knight(self):
         A_file = 0b0111111101111111011111110111111101111111011111110111111101111111
         H_file = 0b1111111011111110111111101111111011111110111111101111111011111110
@@ -269,6 +268,7 @@ class board:
             knight_lookup.append(temp)
             curr = curr<<1
         print(knight_lookup)
+    
     def generate_moves_king(self):
         A_file = 0x7f7f7f7f7f7f7f7f
         H_file = 0b1111111011111110111111101111111011111110111111101111111011111110
@@ -283,11 +283,108 @@ class board:
             king_lookup.append(temp)
             curr = curr << 1 
             self.log_bitboard(temp)
+    
+
+    def move(self,piece,destination,piece_name):
+        assert piece&(piece-1) == 0 and destination&(destination-1) == 0, 'more than one piece found'
+
+        assert self.bitboards[piece_name]&piece != 0, 'no piece found at chosen location'
+        #an attack
+        if self.occupied&destination:
+            for name,bitboard in self.bitboards.items():
+                if bitboard&destination !=0:
+                    self.bitboards[name] &= destination
+
+        self.bitboards[piece_name] &= ~piece
+        self.bitboards[piece_name] |= destination
+    
+    def possible_moves_compiled(self,piece,piece_name):
+
+        piece_index = self.check_bits(piece)
+        base_move = 0XFFFFFFFFFFFFFFFF
+        side = piece_name[:1]
+        piece_name = piece_name[1:]
+
+        match piece_name:
+            case 'pawn':           
+                base_move = self.moves[side+'pawn'][piece_index]
+                
+            case 'knight':
+                base_move = self.moves['knight'][piece_index]
+            
+            case 'queen':
+                base_move = self.possible_moves_cross(piece)|self.possible_moves_diagonals(piece)
+            case 'bishop':
+                base_move = self.generate_mask_diagonal()
+
+            case 'rook':
+                base_move = self.generate_mask_cross()
+
+            case 'king':
+                base_move = self.moves['king'][piece_index]
+            case _:
+                raise SystemExit('Program Exited: No piece found under the name \''+ side + piece_name+'\'')
+            
+        base_move &= self.occupied_sides[side]^self.inside
+        self.log_bitboard(base_move,piece)
+            
+
+
+        
+    
     @staticmethod
     def check_bits(binary):
         for i in range(64):
             if binary>>i == 1:
                 return i 
+    @staticmethod
+    def get_columns(start,end):
+        lst = []
+        for i in range(8):
+            if i<end and i>start:
+                lst.append('0')
+            else:
+                lst.append('1')
+        return int(''.join(lst*8),2)
+    
+    @staticmethod
+    def log_bitboard(*boards):
+        print(boards)
+        for board in boards:
+            print('board',board)
+            assert board & ~0xFFFFFFFFFFFFFFFF == 0
+        symbols = 'XOYZ'
+        square = 0x10000000000000000
+        for rank in range(8):
+            line = ""
+            for file in range(8):
+                square >>= 1
+                square_symbol = "."
+                for i,board in enumerate(boards):
+                    if board & square:
+                        square_symbol = symbols[i]
+
+                line += square_symbol+' '
+            print(line)
+        print('')
+    
+    @staticmethod
+    def format_bin(binaries):
+        return_bin = []
+        if type(binaries) != list:
+            return '{:064b}'.format(binaries)
+        for binary in binaries:
+            return_bin.append('{:064b}'.format(binary))
+        return return_bin        
+            
+
+
+
+            
+
+
+
+
 
 
 
@@ -296,10 +393,5 @@ class board:
         
 b = board()
 
-bits = 0b00000000_00000000_00000000_00000000_00001000_00000000_00000000_00000000
-b.log_bitboard(b.possible_moves_diagonals(bits))
-b.log_bitboard(b.possible_moves_diagonals(bits)&b.occupied)
-bits = 0b00000000_00000000_00000000_00000000_00000000_00001000_00000000_00000000
-b.log_bitboard(b.possible_moves_diagonals(bits))
-b.log_bitboard(b.possible_moves_diagonals(bits)&b.occupied)
-b.log_bitboard(b.occupied)
+b.possible_moves_compiled(0b00000000_00000000_00000000_00000000_00010000_00000000_00000000_00000000,'Wqueen')
+#b.possible_moves_compiled(0b00000000_00000000_00000000_00000000_00000000_00000000_00000000_00001000,'Brook')
